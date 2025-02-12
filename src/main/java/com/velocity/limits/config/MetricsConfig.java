43b2c0c039bf76aabdded5c6ac4bb97bde14b448
Conor <conor@conorjohanlon.com>
2025-02-12 22:04:48 +0000

    Prometheus metrics

--- src/main/java/com/velocity/limits/config/MetricsConfig.java
+++ src/main/java/com/velocity/limits/config/MetricsConfig.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class PrometheusConfig {
+public class MetricsConfig {
     
     @Bean
     public TimedAspect timedAspect(MeterRegistry registry) {
